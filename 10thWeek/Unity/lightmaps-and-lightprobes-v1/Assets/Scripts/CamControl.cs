f1c37219d234b0e7e8af97f978464f4d6b0c741a
hunaynr <hunaynr@gmail.com>
2021-04-30 09:23:33 +0700

    cont of lighting effects -first

---
+++ 10thWeek/Unity/lightmaps-and-lightprobes-v1/Assets/Scripts/CamControl.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamControl : MonoBehaviour
+{
+    GameObject player;
+    Vector3 offset;
+
+    void Start()
+    {
+        offset = transform.position - player.transform.position;
+    }
+
+    void LateUpdate()
+    {
+        float newXPosition = player.transform.position.x - offset.x;
+        float newZPosition = player.transform.position.z - offset.z;
+
+        transform.position = new Vector3(newXPosition, transform.position.y, newXPosition);
+    }
+}
